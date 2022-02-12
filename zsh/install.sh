79267b449b4ea71d0bdbe83f4c82873c8581fd3e
Mahendra Choudhary <pikaatic@gmail.com>
2022-02-12 07:06:31 +0000

    update dotfiles

--- zsh/install.sh
+++ zsh/install.sh
@@ -6,7 +6,6 @@
 . ../helpers.sh
 
 echo_info "Installing ZSH with OH-MY-ZSH..."
-sudo apt install zsh
 
 echo_info "Installing oh-my-zsh..."
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
@@ -15,8 +14,6 @@ echo_info "Installing A lightweight and simple plugin manager for ZSH"
 git clone https://github.com/tarjoilija/zgen.git "${HOME}/.zgen"
 
 echo_info "Symlink .zshrc..."
-rm -rf ${HOME}/.zgen
-rm -rf ${HOME}/.zshrc
 ln -sfT "$HOME/dotfiles/zsh/zshrc" "$HOME/.zshrc"
 
 echo_info "changing shell..."
