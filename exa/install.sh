e5d4a89392b0584bed26fa7a44091ab812d6df6f
Mahendra Choudhary <pikaatic@gmail.com>
2022-02-13 03:21:41 +0000

    update dotfiles

--- exa/install.sh
+++ exa/install.sh
@@ -1,16 +1,16 @@
-#!/bin/bash
+# #!/bin/bash
 
-# shellcheck source=distro.sh
-. ../distro.sh
-# shellcheck source=helpers.sh
-. ../helpers.sh
+# # shellcheck source=distro.sh
+# . ../distro.sh
+# # shellcheck source=helpers.sh
+# . ../helpers.sh
 
-echo_info "Configuring exa..."
+# echo_info "Configuring exa..."
 
-echo_info "Installing cargo..."
-yes | sudo apt install cargo
+# echo_info "Installing cargo..."
+# yes | sudo apt install cargo
 
-echo_info "Installing exa..."
-yes | cargo install exa
+# echo_info "Installing exa..."
+# yes | cargo install exa
 
-echo_done "exa configuration!"
+# echo_done "exa configuration!"
